4e51f5daf39939189776b67ee5a9f26a22aeeafc
eajimenez670 <eajimenez670@gmail.com>
2022-08-26 02:17:36 -0500

    Terminando proyecto de operaciones entre conjuntos

--- src/main/java/org/operations/Button.java
+++ src/main/java/org/operations/Button.java
@@ -3,7 +3,6 @@ package org.operations;
 import org.javatuples.Pair;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -21,10 +20,10 @@ public class Button implements ActionListener {
         return button;
     }
 
-    public String validateOperations(ArrayList<Pair<Integer, Integer>> list) {
-        var setsReflexive = new ArrayList<Pair<Integer, Integer>>();
-        var setsSymmetric = new ArrayList<Pair<Integer, Integer>>();
-        var setsTransitive = new ArrayList<Pair<Integer, Integer>>();
+    public String validateOperationsInt(ArrayList<Pair<Object, Object>> list) {
+        var setsReflexive = new ArrayList<Pair<Object, Object>>();
+        var setsSymmetric = new ArrayList<Pair<Object, Object>>();
+        var setsTransitive = new ArrayList<Pair<Object, Object>>();
 
         for (int i = 0; i < list.size(); i++) {
             // Validate Reflexive
@@ -35,11 +34,40 @@ public class Button implements ActionListener {
             // Validate Symmetric
             for (int j = 0; j < list.size(); j++) {
                 if (i != j) {
-                    if (list.get(i).getValue0() != list.get(i).getValue1()) {
-                        if ((list.get(i).getValue0() == list.get(j).getValue1()) && (list.get(i).getValue1() == list.get(j).getValue0())) {
-                            if (!setsSymmetric.contains(list.get(i)) && !setsSymmetric.contains(list.get(j))) {
-                                setsSymmetric.add(list.get(i));
-                                setsSymmetric.add(list.get(j));
+                    if (
+                            (list.get(i).getValue0() == list.get(j).getValue1()) &&
+                                    (list.get(i).getValue1() == list.get(j).getValue0())
+                    ) {
+                        if (
+                                !setsReflexive.contains(list.get(i)) &&
+                                        !setsSymmetric.contains(list.get(i)) &&
+                                        !setsSymmetric.contains(list.get(j))
+                        ) {
+                            setsSymmetric.add(list.get(i));
+                            setsSymmetric.add(list.get(j));
+                        }
+                    }
+                }
+
+                // Validate Transitive
+                for (int y = 0; y < list.size(); y++) {
+                    if (i != y && j != y) {
+                        if (
+                                (list.get(i).getValue0() == list.get(y).getValue0()) &&
+                                        (list.get(i).getValue1() == list.get(j).getValue0()) &&
+                                        (list.get(j).getValue1() == list.get(y).getValue1())
+                        ) {
+                            if (
+                                    !setsReflexive.contains(list.get(i)) &&
+                                            !setsSymmetric.contains(list.get(i)) &&
+                                            !setsSymmetric.contains(list.get(j)) &&
+                                            !setsTransitive.contains(list.get(i)) &&
+                                            !setsTransitive.contains(list.get(j)) &&
+                                            !setsTransitive.contains(list.get(y))
+                            ) {
+                                setsTransitive.add(list.get(i));
+                                setsTransitive.add(list.get(j));
+                                setsTransitive.add(list.get(y));
                             }
                         }
                     }
@@ -47,6 +75,73 @@ public class Button implements ActionListener {
             }
         }
 
+        return generateMessage(setsReflexive, setsSymmetric, setsTransitive);
+    }
+
+    public String validateOperationsString(ArrayList<Pair<Object, Object>> list) {
+        var setsReflexive = new ArrayList<Pair<Object, Object>>();
+        var setsSymmetric = new ArrayList<Pair<Object, Object>>();
+        var setsTransitive = new ArrayList<Pair<Object, Object>>();
+
+        for (int i = 0; i < list.size(); i++) {
+            // Validate Reflexive
+            if (list.get(i).getValue0().equals(list.get(i).getValue1())) {
+                setsReflexive.add(list.get(i));
+            }
+
+            // Validate Symmetric
+            for (int j = 0; j < list.size(); j++) {
+                if (i != j) {
+                    if (
+                            (list.get(i).getValue0().equals(list.get(j).getValue1())) &&
+                                    (list.get(i).getValue1().equals(list.get(j).getValue0()))
+                    ) {
+                        if (
+                                !setsReflexive.contains(list.get(i)) &&
+                                        !setsSymmetric.contains(list.get(i)) &&
+                                        !setsSymmetric.contains(list.get(j))
+                        ) {
+                            setsSymmetric.add(list.get(i));
+                            setsSymmetric.add(list.get(j));
+                        }
+                    }
+                }
+
+                // Validate Transitive
+                for (int y = 0; y < list.size(); y++) {
+                    if (i != y && j != y) {
+                        if (
+                                (list.get(i).getValue0().equals(list.get(y).getValue0())) &&
+                                        (list.get(i).getValue1().equals(list.get(j).getValue0())) &&
+                                        (list.get(j).getValue1().equals(list.get(y).getValue1()))
+                        ) {
+                            if (
+                                    !setsReflexive.contains(list.get(i)) &&
+                                            !setsSymmetric.contains(list.get(i)) &&
+                                            !setsSymmetric.contains(list.get(j)) &&
+                                            !setsTransitive.contains(list.get(i)) &&
+                                            !setsTransitive.contains(list.get(j)) &&
+                                            !setsTransitive.contains(list.get(y))
+                            ) {
+                                setsTransitive.add(list.get(i));
+                                setsTransitive.add(list.get(j));
+                                setsTransitive.add(list.get(y));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return generateMessage(setsReflexive, setsSymmetric, setsTransitive);
+    }
+
+    public String generateMessage(
+            ArrayList<Pair<Object, Object>> setsReflexive,
+            ArrayList<Pair<Object, Object>> setsSymmetric,
+            ArrayList<Pair<Object, Object>> setsTransitive
+    ) {
+        boolean isEquivalent = false;
         String message = "";
 
         if (setsReflexive.size() > 0) {
@@ -57,13 +152,26 @@ public class Button implements ActionListener {
             message += "Las siguientes propiedades son simetricas: " + getTextMessage(setsSymmetric) + "\n";
         }
 
+        if (setsTransitive.size() > 0) {
+            message += "Las siguientes propiedades son transitivas: " + getTextMessage(setsTransitive) + "\n";
+        }
+
+        if (setsReflexive.size() > 0 && setsSymmetric.size() > 0 && setsTransitive.size() > 0) {
+            message += "El conjunto cuenta con una relación de equivalencia \n";
+            isEquivalent = true;
+        }
+
+        if (setsSymmetric.size() > 0 && setsTransitive.size() > 0 && !isEquivalent) {
+            message += "El conjunto cuenta con una relación de orden estricto \n";
+        }
+
         return message;
     }
 
-    public String getTextMessage(ArrayList<Pair<Integer, Integer>> list) {
+    public String getTextMessage(ArrayList<Pair<Object, Object>> list) {
         String s = "";
 
-        for (Pair<Integer, Integer> pair : list) {
+        for (Pair<Object, Object> pair : list) {
             s += String.format("[%s, %s] ", pair.getValue0(), pair.getValue1());
         }
 
@@ -81,33 +189,64 @@ public class Button implements ActionListener {
         return isValid;
     }
 
+    public void execute(String text, int type) {
+        var sets = text.split("\\|");
+        var listTuples = new ArrayList<Pair<Object, Object>>();
+
+        for (String node : sets) {
+            String nodeTrim = node.trim();
+            if (!validateText(nodeTrim)) {
+                return;
+            }
+
+            var numbersSet = nodeTrim.substring(1, nodeTrim.length() - 1).split(",");
+            if (type == 0) {
+                String num1 = numbersSet[0].trim();
+                String num2 = numbersSet[1].trim();
+
+                if (!num1.matches("[a-zA-Z]+") || !num2.matches("[a-zA-Z]+")) {
+                    throw new StringIndexOutOfBoundsException("No se admiten números");
+                }
+
+                listTuples.add(new Pair<Object, Object>(num1, num2));
+            } else {
+                int num1 = Integer.parseInt(numbersSet[0].trim());
+                int num2 = Integer.parseInt(numbersSet[1].trim());
+                listTuples.add(new Pair<Object, Object>(num1, num2));
+            }
+        }
+        String message = "";
+
+        if (type == 0) {
+            message = validateOperationsString(listTuples);
+        } else {
+            message = validateOperationsInt(listTuples);
+        }
+
+
+        if (message == "") {
+            JOptionPane.showMessageDialog(Design.getFrame(), "El conjunto no cuenta con ninguna propiedad");
+        } else {
+            JOptionPane.showMessageDialog(Design.getFrame(), message);
+        }
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         try {
+            // Extract form data
             String text = Design.getTextField().getText();
-            var sets = text.split("\\|");
-            var listTuples = new ArrayList<Pair<Integer, Integer>>();
-            for (String node : sets) {
-                String nodeTrim = node.trim();
-                if (!validateText(nodeTrim)) {
-                    return;
-                }
-
-                var numbersSet = nodeTrim.substring(1, nodeTrim.length() - 1).split(",");
-                int num1 = Integer.parseInt(numbersSet[0].trim());
-                int num2 = Integer.parseInt(numbersSet[1].trim());
-                listTuples.add(new Pair<Integer, Integer>(num1, num2));
-            }
-
-            var message = validateOperations(listTuples);
-
-            if (message == "") {
-                JOptionPane.showMessageDialog(Design.getFrame(), "El conjunto no cuenta con ninguna propiedad");
-            } else {
-                JOptionPane.showMessageDialog(Design.getFrame(), message);
-            }
-        } catch (Exception ex) {
+            var comboBox = Design.getComboBox().getSelectedIndex();
+            execute(text, comboBox);
+        }
+        catch (IllegalArgumentException ex) {
             JOptionPane.showMessageDialog(Design.getFrame(), "No se admiten letras ni caracteres especiales");
+        }
+        catch (StringIndexOutOfBoundsException ex) {
+            JOptionPane.showMessageDialog(Design.getFrame(), ex.getMessage());
+        }
+        catch (Exception ex) {
+            JOptionPane.showMessageDialog(Design.getFrame(), ex.getMessage());
         }
     }
 }
--- src/main/java/org/operations/Design.java
+++ src/main/java/org/operations/Design.java
@@ -6,16 +6,22 @@ public class Design extends JFrame {
     private static JFrame frame;
     private static JTextField textField;
     private static JLabel label;
+    private static JLabel labelCB;
+    private static JComboBox comboBox;
 
     Design() {
         setTitle("Propiedades de las relaciones entre conjuntos");
-        InitializeLabel(50, 30, 460, 30);
-        InitializeTextField(50, 60, 460, 30);
+        InitializeLabelCB(50, 20, 150, 20);
+        InitializeComboBox(50, 40, 150, 20);
+        InitializeLabel(50, 70, 460, 30);
+        InitializeTextField(50, 95, 460, 30);
+        add(labelCB);
+        add(comboBox);
         add(label);
         add(textField);
-        add(new Button(50, 120, 100, 30).getButton());
-        setSize(550, 240);
+        add(new Button(50, 140, 100, 30).getButton());
         setLayout(null);
+        setSize(550, 260);
         setVisible(true);
     }
 
@@ -27,14 +33,30 @@ public class Design extends JFrame {
         return textField;
     }
 
+    public static JComboBox getComboBox() {
+        return comboBox;
+    }
+
     public void InitializeTextField(int x, int y, int width, int height) {
 
         textField = new JTextField();
         textField.setBounds(x, y, width, height);
     }
 
+    public void InitializeComboBox(int x, int y, int width, int height) {
+        String optionsCB[] = {"Letras", "Números"};
+        comboBox = new JComboBox(optionsCB);
+        comboBox.setBounds(x, y, width, height);
+    }
+
+
     public void InitializeLabel(int x, int y, int width, int height) {
         label = new JLabel("Ingrese el conjunto a validar. (EJ: (1,3) | (2, 3) | ...)");
         label.setBounds(x, y, width, height);
+    }
+
+    public void InitializeLabelCB(int x, int y, int width, int height) {
+        labelCB = new JLabel("Tipo de conjunto");
+        labelCB.setBounds(x, y, width, height);
     }
 }
